05862c010d7dfadae6c8763c92c0c2754cc8f2f0
cbuus <cbuus@unomaha.edu>
2018-02-20 16:08:09 -0600

    test

--- financial/urls.py
+++ financial/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.customer_list, name='customer_list'),
+    url(r'^$',views.home, name='Home'),
+    url(r'^customer', views.customer_list, name='customer_list'),
+    url(r'^customer/detail', views.customer_details, name='customer_detail'),
+
 ]
--- financial/views.py
+++ financial/views.py
@@ -3,10 +3,17 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .models import Customer
 
+def home(request):
+    return render(request, 'financial/home.html', {})
+
 def customer_list(request):
     customers = Customer.objects.filter()
     return render(request, 'financial/customer_list.html', {'customers': customers})
 
-    def customer_detail(request, pk):
-        customer = get_object_or_404(Customer, pk=pk)
-    return render(request, 'customer/customer_detail.html', {'customer': customer})
+def customer_details(request):
+    customers = Customer.objects.filter()
+    return render(request, 'financial/customer_details.html', {'customers': customers})
+
+def stock_list(request):
+    stocks = Stock.objects.filter()
+    return render(request, 'financial/stock_list.html', {'stocks': stocks})
